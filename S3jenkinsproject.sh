8d575371224c4758e4418e948dd8976c7983fe73
namrata4447 <namratamansur@gmail.com>
2022-04-21 10:05:27 +0000

    new S3

---
+++ S3jenkinsproject.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+aws s3api create-bucket --bucket namrata-jobs-bucket --create-bucket-configuration LocationConstraint=us-east-2
+aws s3 cp /home/ubuntu/S3Maven/S3jenkins.txt s3://namrata-jobs-bucket/
